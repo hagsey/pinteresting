842150fb6b4a6170173da6a5b059b7f265ccd21c
Eric Hagemann <hagemann.eric@gmail.com>
2015-09-15 09:47:49 -0400

    Add pins/users association

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150913151637) do
+ActiveRecord::Schema.define(version: 20150915125826) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.string   "user_integer"
+    t.integer  "user_id"
   end
+
+  add_index "pins", ["user_id"], name: "index_pins_on_user_id"
+  add_index "pins", ["user_integer"], name: "index_pins_on_user_integer"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
